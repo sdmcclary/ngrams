cb265189107edc70d8289f32e7e47e8a38cc3115
sdmcclary <sdmcclary@wm.edu>
2024-09-17 19:11:02 -0400

    preprocess1: remove unneeded components

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,33 @@
+#Preprocessing part fo n-grams modeling for java code completion
+#Sean McClary
+
+''' 
+The goal is to make the data more usable by
+    1. Removing duplicates
+    2. Removing unneccesary results from search
+    3. Splitting the data set into the corpus and the test sets
+    
+'''
+
+
 import json
 
-def preprocessing(input_file):
-    with open(input_file, 'r') as file_in:
-        x = 0
-        for obj in file_in:
-            if (x <= 10):
-                print(obj)
-                print('\n')
-                x += 1
-            else:
-                break
+#Building a set of names to ensure no duplication occurs
+unique_names = set()
 
+#Function to remove duplicates and unnecesary information, leaving only name and content from a single version from each project
+def preprocessing(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as p_in, open(output_file, 'w', encoding='utf-8') as p_out:
+        for obj in p_in:#Grabbing each json element individually from the jsonl file
+            json_dict = json.loads(obj)
+            name = json_dict['repo']['name']
+            if name not in unique_names:#checking to ensure we don't already have this project
+                unique_names.add(name)
+                p_out.write(json.dumps({'name': name,'content': json_dict['content']}))
+        #print(unique_names)
+'''My original corpus size was 7GB
+after running through this process,
+the output file is 90MG'''
 
-preprocessing('Corpus/Original.jsonl')
+preprocessing('Corpus/Original.jsonl','Corpus/processedv1.json')
+
